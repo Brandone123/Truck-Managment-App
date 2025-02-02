85f89a9144b2d351d1b5f3fd4e9793dfc691b3de
Brandone Sape <44850152+Brandone123@users.noreply.github.com>
2025-02-02 14:27:56 +0100

    Add files via upload

---
+++ components/maintenance/meterHistory/ImportMeterHistoryDialog.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-dialog :modelValue="modelValue" @update:modelValue="updateModelValue" scrollable max-width="600px">
+    <v-card>
+      <v-toolbar color="primary" dark density="compact">
+        <v-toolbar-title>Import Meter History</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="closeDialog">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <v-select v-model="selectedType" :items="importTypes" label="Select Import Type" variant="outlined"
+          prepend-icon="mdi-file-edit-outline"></v-select>
+        <v-file-input v-model="files" label="Select CSV File" prepend-icon="mdi-cloud-upload-outline"
+          accept=".csv" variant="outlined"></v-file-input>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="primary" @click="closeDialog">Cancel</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click="uploadFile">Upload</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { defineProps, defineEmits } from 'vue';
+import { useMeterHistoryStore } from '@/stores/maintenance/meterHistoryStore';
+//import { useLayoutStore } from '@/stores/layoutStore';
+
+const props = defineProps({
+  modelValue: Boolean,
+});
+
+const emit = defineEmits(['update:modelValue', 'imported']);
+
+const files = ref<File[]>([]);
+const selectedType = ref<string>('tms'); // Default value
+const importTypes = [
+  { title: 'Fleetio', value: 'fleetio' },
+  { title: 'TMS', value: 'tms' }
+];
+
+const meterHistoryStore = useMeterHistoryStore();
+const layoutStore = useLayoutStore();
+
+const updateModelValue = (value: boolean) => {
+  emit('update:modelValue', value);
+};
+
+const closeDialog = () => {
+  emit('update:modelValue', false);
+};
+
+const uploadFile = async () => {
+  if (files.value.length > 0) {
+    const formData = new FormData();
+    formData.append('file', files.value[0]);
+    formData.append('importType', selectedType.value) // Select the first file
+
+    try {
+      layoutStore.setOverlay(true);
+      await meterHistoryStore.importMeterHistory(formData);
+      layoutStore.setStatusMessage('Meter history imported successfully', 'primary');
+      emit('imported');
+    } catch (error) {
+      layoutStore.setStatusMessage('Failed to import meter history', 'error');
+      console.error('Upload error:', error);
+    } finally {
+      layoutStore.setOverlay(false);
+      closeDialog();
+    }
+  } else {
+    console.error('No file selected');
+  }
+};
+</script>
--- components/maintenance/meterHistory/MeterEditDialog.vue
+++ components/maintenance/meterHistory/MeterEditDialog.vue
@@ -1,6 +1,10 @@
 <template>
-  <v-dialog :modelValue="modelValue" @update:modelValue="updateModelValue" max-width="800px">
-    <v-card class="grey-background">
+  <v-dialog
+    :modelValue="modelValue"
+    @update:modelValue="updateModelValue"
+    max-width="800px"
+  >
+    <v-card class="white-background">
       <v-toolbar color="primary" dark density="compact">
         <v-toolbar-title>{{ title }}</v-toolbar-title>
         <v-spacer></v-spacer>
@@ -8,34 +12,61 @@
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-toolbar>
+
       <v-card-text>
-        <v-form ref="meterHistoryForm">
+        <v-form ref="meterHistoryForm" class="pt-4">
           <v-row>
-            <v-col cols="12" md="6">
-              <v-autocomplete v-model="meterHistoryRecord.vehicle_id" item-title="name" item-value="id" variant="solo"
-                flat density="compact" required :items="assetList" label="Vehicle"></v-autocomplete>
+            <!-- Vehicle Selection -->
+            <v-col cols="12" md="12">
+                  <SharedInputVehicleAutoCompleteInput 
+                  variant="outlined" 
+                  flat density="compact"
+                  v-model="meterHistoryRecord.vehicle_id" 
+                  label="Select Vehicle"
+                  :rules="[validation.required]" />
+
+                  <v-select
+                    v-model="meterHistoryRecord.meter_type"
+                    label="Meter Type*"
+                    variant="outlined"
+                    flat
+                    density="compact"
+                    :items="['primary', 'secondary', 'Odometer']"
+                  ></v-select>
+                  <v-text-field
+                    v-model="meterHistoryRecord.meter_value"
+                    label="Mileage*"
+                    variant="outlined"
+                    flat
+                    density="compact"
+                    type="number"
+                    required
+                  >
+                    <template v-slot:append-inner>
+                    </template>
+                  </v-text-field>
+                  <v-text-field
+                    v-model="meterHistoryRecord.meter_date"
+                    label="Meter Date*"
+                    variant="outlined"
+                    flat
+                    density="compact"
+                    type="date"
+                    required
+                  ></v-text-field>
             </v-col>
-            <v-col cols="12" md="6">
-              <v-select v-model="meterHistoryRecord.meter_type" variant="solo" flat density="compact" label="Meter Type"
-                :items="['primary', 'secondary']">
-              </v-select>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field v-model="meterHistoryRecord.meter_value" variant="solo" flat density="compact" required
-                label="Mileage" type="number">
-                <template v-slot:append-inner>
-                  <span>mi</span>
-                </template>
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" md="6">
-              <v-text-field v-model="meterHistoryRecord.meter_date" variant="solo" flat density="compact" required
-                label="Meter Date" type="date">
-              </v-text-field>
-            </v-col>
+            <!-- Additional Section (if needed) -->
+            <!-- <v-col cols="12" md="6">
+              <v-card class="p-4 mt-5">
+                <v-card-title class="font-weight-bold">Additional Information</v-card-title>
+                <v-card-text>
+                </v-card-text>
+              </v-card>
+            </v-col> -->
           </v-row>
         </v-form>
       </v-card-text>
+
       <v-divider></v-divider>
       <v-card-actions>
         <v-btn color="primary" @click="closeDialog">Cancel</v-btn>
@@ -47,29 +78,32 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, watch } from 'vue';
-import { defineProps, defineEmits } from 'vue';
-import { storeToRefs } from 'pinia';
-import type { MeterHistory } from '@/types/maintenance/meterHistoryTypes';
-import { useMeterHistoryStore } from '~/stores/maintenance/meterHistoryStore';
+import { ref, computed, watch } from "vue";
+import { defineProps, defineEmits } from "vue";
+import { storeToRefs } from "pinia";
+import type { MeterHistory } from "@/types/maintenance/meterHistoryTypes";
+import { useMeterHistoryStore } from "~/stores/maintenance/meterHistoryStore";
+import { useValidation } from '~/composables/formValidation';
 
 const assetStore = useAssetStore();
+const validation = useValidation();
 
-const { assetList } = storeToRefs(assetStore)
+const { assetList } = storeToRefs(assetStore);
 
 const props = defineProps({
   modelValue: Boolean,
   asset: {
     type: Object,
-    default: () => ({
-      void: false,
-    } as Partial<MeterHistory>),
+    default: () =>
+      ({
+        void: false,
+      } as Partial<MeterHistory>),
   },
 });
 
-const emit = defineEmits(['update:modelValue', 'save']);
+const emit = defineEmits(["update:modelValue", "save"]);
 
-const meterHistoryForm = ref<HTMLFormElement | null>(null)
+const meterHistoryForm = ref<HTMLFormElement | null>(null);
 
 const meterHistoryRecord = ref({} as Partial<MeterHistory>);
 
@@ -80,19 +114,21 @@ watch(
   }
 );
 
-const title = computed(() => (props.asset && props.asset.id ? 'Edit Meter Entry' : 'Add Meter Entry'));
+const title = computed(() =>
+  props.asset && props.asset.id ? "Edit Meter Entry" : "Add Meter Entry"
+);
 
 const updateModelValue = (value: boolean) => {
-  emit('update:modelValue', value);
+  emit("update:modelValue", value);
 };
 
 const closeDialog = () => {
-  emit('update:modelValue', false);
+  emit("update:modelValue", false);
 };
 
 const saveAsset = () => {
-  emit('save', meterHistoryRecord.value);
-  emit('update:modelValue', false);
+  emit("save", meterHistoryRecord.value);
+  emit("update:modelValue", false);
 };
 </script>
 
